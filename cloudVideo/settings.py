4f8607fabb34bb56a4b48a54f42fc42814e4e76c
Bhuwan Panta <ricky.pantha@gmail.com>
2022-07-21 00:38:57 +0545

    refator: updated the environmental variable in git

--- cloudVideo/settings.py
+++ cloudVideo/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+# read env
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_=#y_8s5t%)_u)9x9o3n(1ct9%+)^(@@p@4e$ncf#uw7^iawe("
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
